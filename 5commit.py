64be308bf97391d15e0cb5d3ac69ce5f56ff29eb
Alessandro Francia <alessandro.francia.info@gmail.com>
2020-11-19 08:07:37 +0100

    commit 5

--- 5commit.py
+++ 5commit.py
@@ -6,3 +6,6 @@ salute = input ("come stai?")
 
 if salute == "molto bene":
     print ("fantastico, sono contento")
+elif salute == "molto male":
+    spiegazione = input ("mi dispiace, puoi spiegarmi perchè?")
+    print ("capisco che possa essere frustrante, ma non ti abbattere così tanto")
